edc6e9cc48a78a4ccf7a97348f691180ad9c83a3
amaan.jain <amaan.jain1996@gmail.com>
2021-05-23 20:36:38 +0530

    Changed UI styles

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,33 +2,33 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#1FC7D4",
-  primaryBright: "#53DEE9",
-  primaryDark: "#0098A1",
-  secondary: "#7645D9",
+  primary: "#aa8929",
+  primaryBright: "#273e04",
+  primaryDark: "#aa8929",
+  secondary: "#8fd129",
   success: "#31D0AA",
   warning: "#FFB237",
 };
 
 export const brandColors = {
-  binance: "#F0B90B",
+  binance: "#273e04",
 };
 
 export const lightColors: Colors = {
   ...baseColors,
   ...brandColors,
-  background: "#FAF9FA",
-  backgroundDisabled: "#E9EAEB",
+  background: "#22310a",
+  backgroundDisabled: "#ed1d23",
   contrast: "#191326",
   invertedContrast: "#FFFFFF",
-  input: "#eeeaf4",
-  inputSecondary: "#d7caec",
-  tertiary: "#EFF4F5",
-  text: "#452A7A",
+  input: "#8fd129",
+  tertiary: "#8fd129",
+  text: "#8fd129",
   textDisabled: "#BDC2C4",
   textSubtle: "#8f80ba",
   borderColor: "#E9EAEB",
-  card: "#FFFFFF",
+  card: "#1b2b03",
+  inputSecondary: "#d7caec",
   gradients: {
     bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
   },
